a02e2893249079614db675a67f93c67b7517f0a0
gosuljo <lurutia@naver.com>
2023-08-19 22:38:12 +0900

    [feat] Car Name Instance 추가

---
+++ src/main/java/racingcar/CarName.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+public class CarName {
+
+    private static final int CAR_NAME_LENGTH_LIMIT_POLICY = 5;
+    private static final String CAR_NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 자동차의 이름이 올바르지 않습니다.";
+
+    private final String carName;
+
+    public CarName(String carName) {
+        isValidCarName(carName);
+        this.carName = carName;
+    }
+
+    private void isValidCarName(String inputCartName){
+        if (inputCartName == null || inputCartName.trim().isEmpty() || inputCartName.length() > CarName.CAR_NAME_LENGTH_LIMIT_POLICY){
+            throw new IllegalArgumentException(CarName.CAR_NAME_LENGTH_ERROR_MESSAGE);
+        }
+    }
+}
---
+++ src/test/java/racingcar/CarNameTest.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class CarNameTest {
+
+    @Test
+    void 자동차_이름_5자_초과_불가_테스트() {
+
+        assertThatThrownBy(() -> {
+            new CarName("abcdee");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 자동차의 이름이 올바르지 않습니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 자동차_이름_0글자_불가_테스트(String input) {
+        assertThatThrownBy(() -> {
+            new CarName(input);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 자동차의 이름이 올바르지 않습니다.");
+    }
+}
